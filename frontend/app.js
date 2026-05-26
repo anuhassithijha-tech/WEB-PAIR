5a1a397fbc81c531ad1742f7b70c693d358c60d3
anuhassithijha-tech <anuhassithijha@gmail.com>
2026-05-26 21:05:39 +0530

    Create app.js

---
+++ frontend/app.js
@@ -0,0 +1,59 @@
+const BACKEND_URL = 'https://YOUR-BACKEND.onrender.com';
+
+async function generateCode() {
+
+    const number = document.getElementById('number').value;
+
+    const result = document.getElementById('result');
+    const loading = document.getElementById('loading');
+    const status = document.getElementById('status');
+
+    result.innerHTML = '';
+    status.innerHTML = '';
+
+    if (!number) {
+        alert('Enter Number');
+        return;
+    }
+
+    loading.innerHTML = 'Generating Pair Code...';
+
+    try {
+
+        const res = await fetch(`${BACKEND_URL}/pair?number=${number}`);
+
+        const data = await res.json();
+
+        loading.innerHTML = '';
+
+        if (!data.status) {
+            result.innerHTML = 'ERROR';
+            return;
+        }
+
+        result.innerHTML = data.code;
+
+        checkStatus(data.id);
+
+    } catch (e) {
+
+        loading.innerHTML = '';
+        result.innerHTML = 'SERVER ERROR';
+    }
+}
+
+async function checkStatus(id) {
+
+    const status = document.getElementById('status');
+
+    const interval = setInterval(async () => {
+
+        try {
+
+            const res = await fetch(`${BACKEND_URL}/status/${id}`);
+            const data = await res.json();
+
+            if (data.connected) {
+
+                status.innerHTML = `
+}
